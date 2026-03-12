c4618d3c836f158b07c803713b76d9952f3e0848
phantanhungpy20061@gmail.com <phantanhungpy20061@gmail.com>
2026-03-12 09:36:25 +0700

    update-version2

--- Single-Responsibility/Program.cs
+++ Single-Responsibility/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var cart = new ShoppingCart();
+        cart.Add(new ShoppingCartItem()
+        {
+            Price = 1200,
+            ProductId = 1,
+            Quantity = 1,
+            Title = "iPhone 12"
+        });
+        cart.Add(new ShoppingCartItem()
+        {
+            Price = 1400,
+            ProductId = 1,
+            Quantity = 2,
+            Title = "iPhone 14"
+        });
+
+        cart.Print();
+
+        Console.WriteLine("Writing to file...");
+        cart.SaveToFile("cart.json");
+
+        cart.Clear();
+        Console.WriteLine($"Now is empty == {!cart.Items.Any()}");
+        cart.Print();
+
+        cart.LoadFromFile("cart.json");
+        Console.WriteLine("Loaded from file");
+        cart.Print();
     }
 }
